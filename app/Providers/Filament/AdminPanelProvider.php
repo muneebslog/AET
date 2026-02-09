bb43fd8733283a8e4fb8b2dd4508251a45ed89c8
Muhammad Muneeb <itsmuneebrehman.conact@gmail.com>
2026-02-09 19:06:30 +0500

    widget

--- app/Providers/Filament/AdminPanelProvider.php
+++ app/Providers/Filament/AdminPanelProvider.php
@@ -32,7 +32,7 @@ class AdminPanelProvider extends PanelProvider
             ])
             ->discoverResources(in: app_path('Filament/Admin/Resources'), for: 'App\Filament\Admin\Resources')
             ->discoverPages(in: app_path('Filament/Admin/Pages'), for: 'App\Filament\Admin\Pages')
-          
+
             ->discoverWidgets(in: app_path('Filament/Admin/Widgets'), for: 'App\Filament\Admin\Widgets')
             ->spa()
             ->widgets([
@@ -55,5 +55,13 @@ class AdminPanelProvider extends PanelProvider
             ->authMiddleware([
                 Authenticate::class,
             ]);
+    }
+
+    protected function getHeaderWidgets(): array
+    {
+        return [
+            \App\Filament\Admin\Widgets\LatestSubscribers::class,
+            \App\Filament\Admin\Widgets\StatsOverview::class,
+        ];
     }
 }
